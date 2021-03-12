585f9944911b5e8e88615f5f6b25ef7b08a42580
serinuntius <wasab1gj@gmail.com>
2021-03-12 23:23:17 +0900

    jpyc burner wallet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import BurnerCore from "@burner-wallet/core";
+import InjectedSigner from "@burner-wallet/core/signers/InjectedSigner";
+import {LocalSigner} from "@burner-wallet/core/signers";
+import {InfuraGateway, XDaiGateway} from "@burner-wallet/core/gateways";
+import { xdai, dai, eth, ERC20Asset } from '@burner-wallet/assets';
+import ModernUI from "@burner-wallet/modern-ui";
+require('dotenv').config();
 
 function App() {
+  const bos = new ERC20Asset({
+    id: 'JPYC',
+    name: 'JPY Coin',
+    network: '100',
+    address: '0x417602f4fbdd471A431Ae29fB5fe0A681964C11b',
+  });
+  const core = new BurnerCore({
+    signers: [new InjectedSigner(), new LocalSigner()],
+    gateways: [new InfuraGateway(process.env.REACT_APP_INFURA_KEY), new XDaiGateway()],
+    assets: [xdai, dai, eth, bos],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ModernUI
+          core={core}
+      />
     </div>
   );
 }
